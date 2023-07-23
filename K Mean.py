1e912998cd8ea611c1d2db07cecf347dddcab5b3
Rizwan-Dard <127917391+Rizwan-Dard@users.noreply.github.com>
2023-07-23 10:45:09 +0500

    Add files via upload

---
+++ K Mean.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+from sklearn import datasets
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+df = pd.read_excel(r'C:\Users\PKAhmadRi1\Desktop\AOA_Resource_Test.xlsx')
+df = df.dropna(axis=0)
+
+
+# In[27]:
+
+
+df.columns = df.columns.str.replace('_', '')
+df.columns = df.columns.str.replace(')', '')
+df.columns = df.columns.str.replace('(', '')
+df.columns = df.columns.str.replace(' ', '')
+df.columns = df.columns.str.replace('/', '')
+df.columns
+
+
+# In[18]:
+
+
+y=df.AverageCrewingFTE
+
+
+# In[28]:
+
+
+X=df.ConversionCostCHFt
+
+
+# In[30]:
+
+
+plt.scatter(X, y)
+plt.show()
+
+
+# In[21]:
+
+
+from sklearn.cluster import KMeans
+
+data = list(zip(X, y))
+inertias = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(data)
+    inertias.append(kmeans.inertia_)
+
+plt.plot(range(1,11), inertias, marker='o')
+plt.title('Elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.show()
+
+
+# In[29]:
+
+
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(data)
+
+plt.scatter(X, y, c=kmeans.labels_)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
